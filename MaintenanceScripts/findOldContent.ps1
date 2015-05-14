06166e928465d5c919226d32791279e701db93a3
Blair Learn <blairlearn@users.noreply.github.com>
2015-05-14 16:21:58 +0000

    Don't allow the script to run out of the box.  Force a human to read it and change the date.

--- MaintenanceScripts/findOldContent.ps1
+++ MaintenanceScripts/findOldContent.ps1
@@ -1,3 +1,4 @@
 # Finds content older than a given date.
 Write-Host -foregroundcolor 'red' "Update the hard-coded date and remove this line before running."
-Get-ChildItem -recurse .\ | ? {$_.PSIsContainer -eq $false -and $_.LastWriteTime -le '8/18/2014 9:00 pm'} | % { $_.FullName }
+# You'll need to delete this comment too, and then uncomment the actual command.
+#Get-ChildItem -recurse .\ | ? {$_.PSIsContainer -eq $false -and $_.LastWriteTime -le '8/18/2014 9:00 pm'} | % { $_.FullName }
