c670bb49b79b2a3a9533400470181f8386254ea5
Blair Learn <blairlearn@users.noreply.github.com>
2015-10-19 20:43:26 +0000

    Gather AppPool information

--- MaintenanceScripts/SiteList.ps1
+++ MaintenanceScripts/SiteList.ps1
@@ -2,37 +2,91 @@ function Main() {
 	# The WebAdministration module requires elevated privileges.
 	$isAdmin = Test-Admin
 	if( $isAdmin ) {
-		Write-Host "Good to go!"
+		Write-Host -foregroundcolor 'green' "Starting..."
 		Import-Module WebAdministration
-		GetSiteNames
+		$OutputFile = GetOutputFile
+		GetSiteNames $OutputFile
+		GetAppPools $OutputFile
 	} else {
 		Write-Host -foregroundcolor 'red' "This script must be run from an AA account."
 	}
 }
 
 
-function GetSiteNames {
-	# Create output file, overwrite an existing one.
-	$OutputFile = GetOutputFilename
-	$junk = New-Item -Name $OutputFile -type File -force
+function GetSiteNames( $OutputFile ) {
+	Write-Host -foregroundcolor 'green' "Collecting site names."
 
-	Add-Content $OutputFile -Value "Name`tID`tPath`tbindings"
+	# Write Headings in a tab-delimited manner.
+	Add-Content $OutputFile -Value "Name`tID`tPath`tbinding"
+
+	# Get a list of sites, with all of their bindings
 	Get-WebSite | foreach {
-		$text = $_.Name + "`t" + $_.ID + "`t" + $_.physicalPath
+		$text = "`"" + $_.Name + "`"`t" + $_.ID + "`t`"" + $_.physicalPath + "`""
 		
 		$_.bindings.collection | foreach {
 			$localText = $text + "`t" + $_
 			Add-Content $OutputFile -Value $localText
 		}
-		
 	}
 
-	Write-Host $fn
+	Write-Host -foregroundcolor 'green' "Done."
 }
 
-function GetOutputFilename {
+function GetAppPools( $OutputFile ) {
+	Write-Host -foregroundcolor 'green' "Collecting site names."
+
+	# Write Headings in a tab-delimited manner.
+	Add-Content $OutputFile -Value "`nName`tPipelineMode`tRuntimeVersion`tApplication"
+
+	Get-ChildItem IIS:\AppPools | foreach {
+		$text = "`"" + $_.Name + "`"`t" + $_.managedPipelineMode + "`t" + $_.managedRuntimeVersion
+		
+		$appList = GetApplicationList $_.Name
+		if( $appList.Length -gt 0 ) {
+			foreach($app in $appList) {
+				$localText = $text + "`t`"" + $app + "`""
+				Add-Content $OutputFile -Value $localText
+			}
+		} else {
+			$text += "`t`"(none)`""
+			Add-Content $OutputFile -Value $text
+		}
+	}
+	
+	Write-Host -foregroundcolor 'green' "Done."
+}
+
+<#
+	The list of applications which use a given appPool isn't a property of the
+	appPool itself.  This information is actually calculated from the system configuration.
+	GetApplicationList finds the sites and applications belonging to $appPoolName and
+	returns a list of their names.
+	
+	Based on: http://stackoverflow.com/a/20751426/282194
+#>
+function GetApplicationList( $appPoolName ) {
+	$pn = $appPoolName
+    $sites = get-webconfigurationproperty "/system.applicationHost/sites/site/application[@applicationPool=`'$pn`'and @path='/']/parent::*" machine/webroot/apphost -name name
+    $apps = get-webconfigurationproperty "/system.applicationHost/sites/site/application[@applicationPool=`'$pn`'and @path!='/']" machine/webroot/apphost -name path
+    $arr = @()
+	$output = @()
+    if ($sites -ne $null) {$arr += $sites}
+    if ($apps -ne $null) {$arr += $apps}
+    if ($arr.Length -gt 0) {
+      foreach ($s in $arr) { $output += $s.Value }
+    }
+
+	return $output
+}
+
+function GetOutputFile {
 	$Computer = Get-WmiObject -Class Win32_ComputerSystem
-	return $Computer.Name + ".txt"
+	$name = $Computer.Name + ".txt"
+
+	# Create output file, overwrite an existing one.
+	$junk = New-Item -Name $name -type File -force
+
+	return $name
 }
 
 
