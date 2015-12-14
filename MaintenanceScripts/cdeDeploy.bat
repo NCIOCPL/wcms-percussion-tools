40bbaaa5181c350e6864ee32e5665e95a1ae6b44
Blair Learn <blairlearn@users.noreply.github.com>
2015-12-14 18:01:43 +0000

    Perform a backup before deploying.

--- MaintenanceScripts/cdeDeploy.bat
+++ MaintenanceScripts/cdeDeploy.bat
@@ -1,4 +1,6 @@
 @echo off
+rem Do a backup first.
+powershell -ExecutionPolicy RemoteSigned ./cdeBackup.ps1
 setLocal
 rem Build up the source location from the current execution path and
 rem an assumed DATA subdirectory.
