67e6ce6a246b53da47fc4950cffaefdb2860e09f
hoay <webcommauto@users.noreply.github.com>
2016-01-19 16:42:36 +0000

    change source location

--- MaintenanceScripts/cdeDeploy.bat
+++ MaintenanceScripts/cdeDeploy.bat
@@ -4,6 +4,6 @@ powershell -ExecutionPolicy RemoteSigned ./cdeBackup.ps1
 setLocal
 rem Build up the source location from the current execution path and
 rem an assumed DATA subdirectory.
-set filepath=%~dp0DATA
+set filepath=%~dp0CDECode
 powershell -ExecutionPolicy RemoteSigned ./cdeDeploy.ps1 %filepath%
 pause
--- MaintenanceScripts/gkDeploy.bat
+++ MaintenanceScripts/gkDeploy.bat
@@ -4,6 +4,6 @@ powershell -ExecutionPolicy RemoteSigned ./cdeBackup.ps1
 setLocal
 rem Build up the source location from the current execution path and
 rem an assumed DATA subdirectory.
-set filepath=%~dp0DATA
+set filepath=%~dp0GKCode
 powershell -ExecutionPolicy RemoteSigned ./gkDeploy.ps1 %filepath%
 pause
