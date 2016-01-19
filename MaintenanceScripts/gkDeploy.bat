fee07ac2ccee7d3a0d98c32865b2c138af3006dc
hoay <webcommauto@users.noreply.github.com>
2016-01-19 16:36:05 +0000

    change to one script backup first then deploy from L:\gkdeploy

--- MaintenanceScripts/gkDeploy.bat
+++ MaintenanceScripts/gkDeploy.bat
@@ -1,3 +1,9 @@
 @echo off
-powershell -ExecutionPolicy RemoteSigned ./gkDeploy.ps1 %1
+rem Do a backup first.
+powershell -ExecutionPolicy RemoteSigned ./cdeBackup.ps1
+setLocal
+rem Build up the source location from the current execution path and
+rem an assumed DATA subdirectory.
+set filepath=%~dp0DATA
+powershell -ExecutionPolicy RemoteSigned ./gkDeploy.ps1 %filepath%
 pause
